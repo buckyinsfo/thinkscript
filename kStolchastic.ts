b4fa673c77bf6ec7cfaaaf820778457e34568301
Timothy J. Sayre <tsayre@cachehive.com>
2014-05-16 00:59:54 -0700

    Changed enum for selecting smoothing method to def.  Use this to test internally for whcih method wlorks best.

--- kStolchastic.ts
+++ kStolchastic.ts
@@ -16,11 +16,11 @@
 
 declare lower;
 input KPeriod = 13;
-input smoothingType = { default _SMA, _EMA, _EMA2, _WMA};
 
 def priceH = high;
 def priceL = low;
 def priceC = close;
+def smoothingType = { default _SMA, _EMA, _EMA2, _WMA};
 
 
 def over_bought = 80;
@@ -34,20 +34,24 @@ def FastK = c1 / c2 * 100;
 
 plot FullK;
 plot FullD;
-## Hide the trigger line to de-clutter the chart.
+## Hide the trigger line to declutter the chart.
 FullD.Hide();
 
 switch ( smoothingType ){
     case _SMA:
+        smoothingType = smoothingType._SMA;
         FullK = Average(FastK, slowing_period);
         FullD = Average(FullK, DPeriod); 
     case _EMA:
+        smoothingType = smoothingType._EMA;
         FullK = ExpAverage(FastK, slowing_period);
         FullD = ExpAverage(FullK, DPeriod);
      case _EMA2:
+        smoothingType = smoothingType._EMA2;
         FullK = EMA2(FastK, slowing_period, 0.2);
         FullD = EMA2(FullK, DPeriod, 0.2);
     case _WMA:
+        smoothingType = smoothingType._WMA;
         FullK = WMA(FastK, slowing_period);
         FullD = WMA(FullK, DPeriod);
 }
