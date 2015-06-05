a507ea1d2e3639f69756e4c1fc9f26b6711a60c8
Timothy Sayre <tim@cachehive.com>
2015-06-04 23:19:02 -0700

    Incremental commit for safe keeping.  Much testing to do.

--- kAwakeSTUDY.ts
+++ kAwakeSTUDY.ts
@@ -20,7 +20,7 @@
 ##################################################################################
 declare lower;
 
-input triggerLevel = 20;     ## The level at which to trigger an alert.  
+input trigger_level = 20;     ## The level at which to trigger an alert.  
 input length = 14;           ## Number of bars to be used in the ADX calculation.
 
                              ## Set to true if it is desired for ADX indicator to be
@@ -31,19 +31,17 @@ input length = 14;           ## Number of bars to be used in the ADX calculation
 ## TODO - Create a separate plot with only one color and use the SetHiding flag to flip between the two.
 ##input usePlotColoring = {default TRUE, FALSE};
 
-                             ## TODO Explain overlap?
-input length = 14;
-input trigger_level = 20;
 
 input showOverLap = {default TRUE, FALSE};
-input showAdjustedADX = {default TRUE, FALSE};
+input showAdjustedADXPlot = {default TRUE, FALSE};
+input showADXLabel = { default TRUE, FALSE };
 
 def ADX = reference DMI(length, AverageType.WILDERS).ADX;
 
 ## Allow user input to show/hide the adjusted ADX plot.
 plot adjusted_ADX = (ADX - trigger_level) / 100;
 adjusted_ADX.AssignValueColor( if adjusted_ADX >= 0 then Color.UPTICK else Color.DOWNTICK );
-adjusted_ADX.SetHiding( !showAdjustedADX );
+adjusted_ADX.SetHiding( !showAdjustedADXPlot );
 
 ## Determine awake_factor based on comparing ADX to trigger level
 def awake_factor;
@@ -61,32 +59,32 @@ State.DefineColor("Trigger", Color.CYAN);
 State.DefineColor("Asleep", Color.RED);
 
 ## Override the color of the plot line for State plot based 
-## on momentum between previous and current bars.
-def halfDiff = (high - low) * .5;
-def pull_back = high < high[1] - halfDiff[1];
-def push_up = low > low[1] + halfDiff[1];
-def hard_change = pull_back or push_up;
-
-State.AssignValueColor( if hard_change then Color.WHITE else if State >= 0 then State.Color("Awake") else if State == 0 then State.Color("Trigger") else State.Color("Asleep"));
+## on extreme inside move compared to previous bar.
+def half_diff = (high - low) / 2;
+def extreme_inside;
+if  low >= low[1] and high > low then {
+    extreme_inside = ( (high[1] - half_diff[1]) - (low[1] + half_diff) ) / (high - low) + (low - low[1]);
+} else {
+    extreme_inside = 0;  
+}
+State.AssignValueColor( if extreme_inside >= 1 then Color.WHITE else if State >= 0 then State.Color("Awake") else if State == 0 then State.Color("Trigger") else State.Color("Asleep"));
 State.SetLineWeight( 3 );
 AddCloud( awake_factor, -awake_factor, Color.YELLOW, Color.DARK_GRAY );
 
 ## Determine overlap plot
 ## Calculate if the current bar is inside the previous bar.
-
-def OverLap;
-def prev_bar = high[1] - low[1] > 0;
-if (prev_bar && low > low[1]) then {
-    OverLap = (low - low[1]) / (high[1] - low[1]);
-} else if (prev_bar && high < high[1]) then {
-    OverLap = (high[1] - high) / (high[1] - low[1]);
+def over_lap;
+if  low >= low[1] and high > low then {
+    over_lap = (high[1] - low[1]) / (high - low) + (low - low[1]);
 } else {
-    OverLap = 0;  
+    over_lap = 0;  
 }
-plot OLP = OverLap;
-OLP.AssignValueColor( if OverLap >= 1 then Color.WHITE else Color.BLACK );
+plot OLP = over_lap;
+OLP.AssignValueColor( if over_lap >= 1 then Color.WHITE else Color.BLACK );
 OLP.SetHiding( !showOverLap );
 
+## Add a label to the chart with ADX.
+AddLabel(showADXLabel, concat( "ADX = ", ADX ));
 
 ## TODO
 #if Value1 <> Value1[1] then
@@ -98,9 +96,9 @@ OLP.SetHiding( !showOverLap );
 #    Value2 = TL_SetStyle(Value2, 5);
 #}
 
-
 #######################################################
 # Alert Section
 #######################################################
-##def alert_trigger = { default alert_off, bull_rev, bear_rev };
-##def direction = { default up_tick, down_tick };
+alert( adjusted_ADX crosses above 0, concat( GetSymbol(), " - Market is awaking up."), Alert.BAR, Sound.CHIMES );
+alert( adjusted_ADX crosses below 0, concat( GetSymbol(), " - Market is going to sleep."), Alert.BAR, Sound.CHIMES );
+#######################################################
