55d9e66b65bae58f72551084b8779099fff8e7e5
Timothy Sayre <tim@cachehive.com>
2015-06-19 11:11:41 -0700

    Cleaned up script for release.  Removed references to features planned in version 2.  We should be coming to the end of development on this branch.

--- kAwakeSTUDY.ts
+++ kAwakeSTUDY.ts
@@ -5,7 +5,7 @@
 #  This code is based on J. Welles Wilder's ADX indicator.  It is combined with an  
 #  analysis of price pull back from the previous day to indicate strength of trend.
 #  It is designed to vary background colors to indicate "awake" or "asleep" state 
-#  of the asset.  White -> Sunny/Awake & Dark Gray -> Night/Asleep
+#  of the asset.  Yellow -> Sunny/Awake & Dark Gray -> Night/Asleep
 #
 #  The coloring goes from Yellow to White "Hot" when price moves at such a rate 
 #  that we see less than 50% pull backs --market trend is very strong.  
@@ -36,9 +36,8 @@ input length = 14;           ## Number of bars to be used in the ADX calculation
 ##input usePlotColoring = {default true, false};
 
 input showOverLap = {default true, false};
-input showAdjustedADXPlot = { true, default false};
+##input showAdjustedADXPlot = { true, default false};
 input showADXLabel = { default true, false };
-##input overlap_threshold= 1.0; ## used to trigger overlap bubbles based on the overlap_threshold >= gap up/down of a bar compared to previous bar.  May want to change this to show 2.0 or 3.0 times previous bar length. 
 
 ## Check if bar location is in expansion area.
 def onExpansion = if isNaN( close ) then yes else no;
@@ -49,25 +48,26 @@ def ADX = reference DMI(length, AverageType.WILDERS).ADX;
 def awake_factor;
 if ( onExpansion ) then {
     awake_factor = Double.NaN;
-} else if ( ADX > trigger_level ) then {        ## Above trigger line
+} else if ( ADX >= trigger_level + 1 ) then {       ## Above trigger line
     awake_factor = 0.5;
-} else if (ADX == trigger_level) then {         ## At trigger line
+} else if (ADX < trigger_level + 1) and
+          (ADX > trigger_level)  then {             ## At trigger line
     awake_factor = 0.0;
-} else {                                        ## Below trigger line
+} else {                                            ## Below trigger line
     awake_factor = -0.5;
 }
 
 plot State = awake_factor;
-## Allow user input to show/hide the adjusted ADX plot.
-plot adjusted_ADX = (ADX - trigger_level) / 100;
-adjusted_ADX.AssignValueColor( if adjusted_ADX >= 0 then Color.UPTICK else Color.DOWNTICK );
-adjusted_ADX.SetHiding( showAdjustedADXPlot );
 
 ## These color are for the line plot, not the background
-State.DefineColor("Awake", Color.WHITE);
+State.DefineColor("Awake", Color.YELLOW);
 State.DefineColor("Trigger", Color.CYAN);
 State.DefineColor("Asleep", Color.RED);
 
+## Background coloring ( YELLOW -> Sunny/Awake & Dark_Gray -> Night/Asleep
+AddCloud( awake_factor, -awake_factor, Color.YELLOW, Color.DARK_GRAY );
+
+## Enhancement 1 to ADX study.
 ## Override the color of the plot line for State plot based 
 ## on extreme inside move compared to previous bar.
 def half_diff = (high - low) / 2;
@@ -77,56 +77,44 @@ if  low >= low[1] and high > low then {
 } else {
     extreme_inside = no;  
 }
-State.AssignValueColor( if extreme_inside  then Color.WHITE else if State >= 0 then State.Color("Awake") else if State == 0 then State.Color("Trigger") else State.Color("Asleep"));
-State.SetLineWeight( 3 );
 
-## Background coloring ( White -> Sunny/Awake & Dark_Gray -> Night/Asleep
-AddCloud( awake_factor, -awake_factor, Color.WHITE, Color.DARK_GRAY );
-
+## Enhancement 2 to ADX study.
 ## Determine overlap plot
 ## Calculate if the current bar is inside the previous bar.
 def over_lap;
-##def gap_up;
 if low > low[1] then {
     over_lap = (low - low[1]) / ( high[1] - low[1]);
-##    gap_up = yes;
 } else if high < high[1] then {
     over_lap = (high[1] - high) / (high[1] - low[1]);
-##    gap_up = no;
 } else {
     over_lap = Double.NaN;
-##    gap_up = Double.NaN;
 }
 
-## Plot over_lap value as a down arrow.
+def enhanced_condition = if extreme_inside or over_lap >= 1 then yes else no;
+State.AssignValueColor( if enhanced_condition  then Color.WHITE else if State > 0 then State.Color("Awake") else if State == 0 then State.Color("Trigger") else State.Color("Asleep"));
+State.SetLineWeight( 5 );   ## Range from 1 to 5 ThinkScript imposed.
+
+## Plot over_lap value
 ## Ignore over_lap for asleep state.
 plot OLP = over_lap - 0.5;    
 OLP.AssignValueColor( if over_lap >= 1 then Color.WHITE else Color.BLACK );
 OLP.SetPaintingStrategy( PaintingStrategy.LINE );
 OLP.SetHiding( showOverLap );
-##AddChartBubble( if over_lap >= overlap_threshold then yes else no, 0.5, Round( over_lap, 2 ) + "x", Color.WHITE );
-##AddVerticalLine( if over_lap >= overlap_threshold then yes else no, if gap_up then "Gap Up" else "Gap Down", Color.WHITE, Curve.SHORT_DASH );
  
 ## Debug bubbles
+## used to trigger overlap bubbles based on the overlap_threshold >= gap up/down of a bar compared to previous bar.  
+## May want to change this to show 2.0 or 3.0 times previous bar length. 
+## input overlap_threshold= 1.0; 
 ##AddChartBubble( if over_lap >= overlap_threshold then yes else no, over_lap, over_lap + " on high of " + high + " and h[1] of " + high[1]);
 ##AddChartBubble( if over_lap >= overlap_threshold then yes else no, over_lap, over_lap + " on low of " + low );
 
 ## Add a label to the chart with ADX.
 AddLabel( !showADXLabel, concat( "ADX = ", ADX ));
 
-## TODO - What does this do?
-#if Value1 <> Value1[1] then
-#{
-#    Value2 = TL_New(Date, Time, High+1, Date, Time, Low);
-#    TL_SetExtRight(Value2, True);
-#    TL_SetExtLeft(Value2, True);
-#    TL_SetColor(Value2, VerticalLineColor);  //The color 100 is a very dark red
-#    Value2 = TL_SetStyle(Value2, 5);
-#}
-
 #######################################################
 # Alert Section
 #######################################################
+def adjusted_ADX = (ADX - trigger_level) / 100;
 alert( adjusted_ADX crosses above 0, concat( GetSymbol(), " - Market is awaking up."), Alert.BAR, Sound.CHIMES );
 alert( adjusted_ADX crosses below 0, concat( GetSymbol(), " - Market is going to sleep."), Alert.BAR, Sound.CHIMES );
 #######################################################
