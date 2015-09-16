a69d10214fb28b746a3e0d164466780de267af48
Timothy Sayre <tim@cachehive.com>
2015-09-15 23:51:48 -0700

    Interim safety commit.  Have fold function and charting working.  Still need to get alignment working.

--- kJenkinsSTUDY.ts
+++ kJenkinsSTUDY.ts
@@ -3,19 +3,19 @@ declare lower;
 input jenkins_range = 1100000000;
 input aggregationPeriod = AggregationPeriod.DAY;
 
-def bar_vol = volume( "spy", aggregationPeriod );
+def bar_vol = GetValue( volume, 0 );
 def cycle_rev;
 def cumulative_vol;
 def add_label;
 
-if IsNaN( cumulative_vol[1] )
-then {
-    ## Account initial state.
+if IsNaN( cumulative_vol[1] ) then {
+    ## Account for initial state or expansion are.
+    ##cumulative_vol = temp % jenkins_range;
     cumulative_vol = bar_vol;
     cycle_rev = 1;
-    
+
     ## debug
-    add_label = no;
+    add_label = yes;
 } else if ( cumulative_vol[1] + bar_vol > jenkins_range )
 then {
     ## Account for cycle reversal.
@@ -30,7 +30,7 @@ then {
     }
     
     ## debug
-    add_label = yes;
+    add_label = no;
 } else {
     cumulative_vol = cumulative_vol[1] + bar_vol;
     cycle_rev = cycle_rev[1];
@@ -43,8 +43,6 @@ plot VolumeDivided =  If (cycle_rev == 1, 0, 1) + cycle_rev * cumulative_vol / j
 ##plot VolumeDivided =  cumulative_vol / jenkins_range;
 VolumeDivided.SetPaintingStrategy(PaintingStrategy.TRIANGLES);
 
-AddChartBubble( add_label, 1.0, AsText(cycle_rev, NumberFormat.TWO_DECIMAL_PLACES), Color.WHITE );
-
 
 input length = 252;
 
@@ -53,18 +51,26 @@ def lo_bar = GetMinValueOffset(low, length);
 def hi_hi = HighestAll( high );
 def lo_lo = LowestAll( low );
 
-def off_set = Min( hi_bar, lo_bar );
+def off_set = length - hi_bar; ##Min( hi_bar, lo_bar );
 
 ## use more recent to anchor the jenkins cycle.
 ##if ( off_set == hi_bar ) then {
-def vol_to_hi = fold i = 0 to off_set with vol = 0 do vol + GetValue( volume, i );
+
+def label;
+def vol_to_hi;
+if isNaN( vol_to_hi[1] ) then {
+    vol_to_hi = fold i = 0 to off_set with vol = 0 do vol + GetValue( volume, i );
+    label = yes;
+} else {
+    vol_to_hi = -1.0;
+    label = no;
+}
 
 
-    
-    
-    
 
-
+#### debug ####    
+##AddChartBubble( add_label, 1.0, AsText(cycle_rev, NumberFormat.TWO_DECIMAL_PLACES), Color.WHITE );
+AddChartBubble( label, 0.2, AsText(vol_to_hi, NumberFormat.TWO_DECIMAL_PLACES), Color.BLUE, no );
 
 AddLabel( yes, "Lowest Offset: " + lo_bar + " Highest Offset: " + hi_bar, Color.PLUM);
 AddLabel( yes, "Lowest: " + lo_lo + " Highest: " + hi_hi + "Volume - " + vol_to_hi, Color.PLUM);
