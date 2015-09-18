b417d6566c762ea7f1765fdfedc29875bf37621b
Timothy Sayre <tim@cachehive.com>
2015-09-18 14:53:47 -0700

    Final version to be presented at the 9/19/2015 San Diego Investools Meeting.

--- kJenkinsSTUDY.ts
+++ kJenkinsSTUDY.ts
@@ -1,15 +1,31 @@
+#hint: <b>Jenkins Cycle Volume</b> \n This study indicates a possible trend reversal after the cummulative volume reached the user specified range.\n
+#hint jenkins_range: The number of shares traded to cause a trend reversal.
+#hint length: the number of trading days per year.
+##################################################################################
+#
+#  This code is based on some of the cyclic and harmonic methodology theory developed 
+#  by Michael S. Jenkins.  Ken Hodor modified this cyclic indicator based on trade volume 
+#  of the SPY and implemented his approach in TradeStation.  This is an implementation 
+#  of the approach Ken presented at the August 22, 2015 San Diego Investools Meeting.
+#
+#  Every 1.1 Billion shares of SPY traded represents a possible trend reversal.
+#
+#  Original Implementation: Ken Hodor - TradeStation Easy Language
+#  09/16/2015 Implemented by Tim Sayre - ThinkorSwim thinkScript
+#
+#   NO ACTUAL OR IMPLIED WARRANTIES OFFERED - USE AT YOUR OWN DISCRETION 
+##################################################################################
 declare lower;
 
-input jenkins_range = 1100000000;
-input aggregationPeriod = AggregationPeriod.DAY;
+input jenkins_range = 1100000000;  ## Ken Hodor determined 1.1 billion shares traded fit the data set.
+def length = 252;                  ## Trading days per year.
+input showApproxDaysRemainLabel = { default yes, no };  ## Shows/Hide Label with approximate number of bars until reversal.
+
+def cycle_dir;
+def cumulative_vol;
+def debug_label;
 
 def bar_vol = GetValue( volume, 0 );
-
-def length = 252;
-def cycle_rev;
-def cumulative_vol;
-def add_label;
-def bars_per_cycle;
 
 ## Only need these value at the first bar, but not sure how else to keep this tidy by 
 ## using the Compound Value function w/o doing it for every bar.  the variable shouldn't 
@@ -17,39 +33,38 @@ def bars_per_cycle;
 def avg_daily = ( fold i = 0 to length with vol_sum = 0 do vol_sum + GetValue( volume, i ) ) / length;
 def lo_idx = GetMinValueOffset( low, length );
 def vol_to_low = CompoundValue( 1, vol_to_low[1], fold j = 0 to length - lo_idx with vol = 0 do vol + GetValue( volume, j ) );
-def cur_dir =  CompoundValue( 1, cycle_rev[1], If( floor( vol_to_low / jenkins_range ) % 2 == 0, 1, -1 ) );
 
-## If first bar then calculate based on chart lowest low.  
-## Otherwise use the previous bars value for cumulative volume.
+ ## Checks to see if odd even number to determine initial direction.
+ def cur_dir =  CompoundValue( 1, cycle_dir[1], If( floor( vol_to_low / jenkins_range ) % 2 == 0, 1, -1 ) );
+
+## If first bar we need to calculate initial volume based on location of lowest low bar.  
+## Otherwise we use the previous bars value for cumulative volume.
 def hist_vol = CompoundValue( 1, cumulative_vol[1],  vol_to_low % jenkins_range );
 
+## Check to see if we need to reverse direction or njot.
 if ( hist_vol + bar_vol > jenkins_range ) then {
     ## Account for cycle reversal.
     cumulative_vol = hist_vol + bar_vol - jenkins_range;
-    cycle_rev = cur_dir * (-1);
-    bars_per_cycle = 0;
+    cycle_dir = cur_dir * (-1);
     
     ## debug
-    add_label = no;
+    debug_label = no;
 } else {
     cumulative_vol = hist_vol + bar_vol;
-    cycle_rev = cur_dir;
-    bars_per_cycle = CompoundValue( 1, bars_per_cycle[1], 0 ) + 1; 
+    cycle_dir = cur_dir;
+
     ## debug
-    add_label = no;
+    debug_label = no;
 }
 
-plot VolumeDivided =  If (cycle_rev == 1, 0, 1) + cycle_rev * cumulative_vol / jenkins_range;
-VolumeDivided.SetPaintingStrategy(PaintingStrategy.LINE_VS_POINTS);
+plot JenkinsVol =  If (cycle_dir == 1, 0, 1) + cycle_dir * cumulative_vol / jenkins_range;
+JenkinsVol.SetPaintingStrategy(PaintingStrategy.LINE_VS_POINTS);
+JenkinsVol.SetLineWeight( 2 );   ## Range from 1 to 5 ThinkScript imposed.
 
-#### debug ####    
-##AddChartBubble( add_label, 1.0, AsText(cycle_rev, NumberFormat.TWO_DECIMAL_PLACES), Color.WHITE );
-##AddChartBubble( label, 0.2, AsText(init_vol, NumberFormat.TWO_DECIMAL_PLACES), Color.BLUE, no );
-
-##def temp_dir = floor( vol_to_low / jenkins_range ) % 2;
-##AddLabel( yes, "Temp Dir - " + temp_dir, Color.PLUM);
-
+## Display for approximate days remaining in the cycle.
 def remain = ( jenkins_range - cumulative_vol ) / avg_daily;
-AddLabel( yes, "Approx. Days Remaining - " + Round( remain, 2 ), Color.PLUM);
+AddLabel( showApproxDaysRemainLabel, "Approx. Days Remaining - " + Round( remain, 2 ), Color.WHITE);
 
-AddLabel( yes, "Volume to Low - " + vol_to_low, Color.PLUM);
+#### debug #### 
+## Just change this out for other variables.   
+AddChartBubble( debug_label, 1.0, AsText( cycle_dir, NumberFormat.TWO_DECIMAL_PLACES ), Color.WHITE );
