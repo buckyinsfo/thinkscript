9337ebf32bb7cf46841244978571003bfa0160c7
Timothy Sayre <buckyinsfo@gmail.com>
2016-11-18 16:58:22 -0800

    Updated to include phantom buy, vertical lines at inflection points and silence audible alert switch.

--- kStochasticSTUDY.ts
+++ kStochasticSTUDY.ts
@@ -19,18 +19,24 @@
 # 10/10/2016 updated by Tim Sayre - Updated the smoothing 
 #            constants and added vertical lines to match the
 #            TradeStation implementation.
+# 11/18/2016 updated by Tim Sayre - Added Phantom buy support
+#            and capability to turn off audible alerts.
+#
 #######################################################
 
 declare lower;
 input kPeriod = 13;
-input showTradeLines = yes;
+input showTradeLines = yes;   ## Draw vertical lines at critical inflection points
+input audibleAlerts = yes;    ## Audible alerts at inflection points
+input phantomBuyAlert = yes;  ## Trigger inflection point on phantom buy.
 
 def priceH = high;
 def priceL = low;
 def priceC = close;
 
-def over_bought = 80;
-def over_sold = 20;
+def over_bought_level = 80;
+def mid_range_level = 50;
+def over_sold_level = 20;
 
 def smoothConstK = Max( 3, RoundDown( kPeriod * 0.6, 0 ));    ## constant for smoothing fast K line.
 def smoothConstD = Max( 2, RoundDown( kPeriod / 20, 0 ));     ## constant for smoothing fast D line.
@@ -38,6 +44,9 @@ def smoothConstD = Max( 2, RoundDown( kPeriod / 20, 0 ));     ## constant for sm
 def c1 = priceC - Lowest(priceL, kPeriod);
 def c2 = Highest(priceH, kPeriod) - Lowest(priceL, kPeriod);
 def FastK = c1 / c2 * 100;
+
+plot OverBought = over_bought_level;
+plot OverSold = over_sold_level;
 
 plot FullK;
 plot FullD;
@@ -48,11 +57,11 @@ FullD.Hide();
 FullK = MovingAverage(AverageType.SIMPLE, FastK, smoothConstK);
 FullD = MovingAverage(AverageType.SIMPLE, FullK, smoothConstD);
 
-plot OverBought = over_bought;
-plot OverSold = over_sold;
-
+#######################################################
+# Analyze State Section
+#######################################################
 def trend = { default up, down, bull_rev, bear_rev };
-if ( FullK >= over_bought  ) {
+if ( FullK >= over_bought_level  ) {
     if ( trend[1] == trend.bull_rev )
     then {
         trend = trend.bull_rev;
@@ -67,7 +76,7 @@ if ( FullK >= over_bought  ) {
         }
     }
 }
-else if ( FullD <= over_sold ) {
+else if ( FullK <= over_sold_level ) {
     if ( trend[1] == trend.bear_rev )
     then {
         trend = trend.bear_rev;
@@ -122,22 +131,82 @@ else {
     alert_trigger = alert_trigger.alert_off;
 }
 
-Alert( alert_trigger == alert_trigger.bull_rev, Concat( GetSymbol(), " - Bull trend reversal"), Alert.BAR, Sound.Ring );
-Alert( alert_trigger == alert_trigger.bear_rev, Concat( GetSymbol(), " - Bear trend reversal"), Alert.BAR, Sound.Ring );
-
-Alert( FullK crosses below over_bought, Concat( GetSymbol(), " - Crossed below over bought level"), Alert.BAR, Sound.Ring );
-Alert( FullK crosses above over_sold, Concat( GetSymbol(), " - Crossed above over sold level"), Alert.BAR, Sound.Ring );
 ######################################################
+# Alerts that provide a text message in the 
+# Message Center and the audible sound.
+######################################################
+Alert( alert_trigger == alert_trigger.bull_rev && audibleAlerts == yes, Concat( GetSymbol(), " - Bull trend reversal"), Alert.BAR, Sound.Ring );
+Alert( alert_trigger == alert_trigger.bear_rev && audibleAlerts == yes, Concat( GetSymbol(), " - Bear trend reversal"), Alert.BAR, Sound.Ring );
+Alert( FullK crosses below over_bought_level && audibleAlerts == yes, Concat( GetSymbol(), " - Crossed below over bought level"), Alert.BAR, Sound.Ring );
+Alert( FullK crosses above over_sold_level && audibleAlerts == yes, Concat( GetSymbol(), " - Crossed above over sold level"), Alert.BAR, Sound.Ring ); 
+######################################################
+
+
+######################################################
+# Alerts that provide a text message in the 
+# Message Center ONLY NO audible sound.
+######################################################
+Alert( alert_trigger == alert_trigger.bull_rev && audibleAlerts == no, Concat( GetSymbol(), " - Bull trend reversal"), Alert.BAR, Sound.NoSound );
+Alert( alert_trigger == alert_trigger.bear_rev && audibleAlerts == no, Concat( GetSymbol(), " - Bear trend reversal"), Alert.BAR, Sound.NoSound );
+Alert( FullK crosses below over_bought_level && audibleAlerts == no, Concat( GetSymbol(), " - Crossed below over bought level"), Alert.BAR, Sound.NoSound );
+Alert( FullK crosses above over_sold_level && audibleAlerts == no, Concat( GetSymbol(), " - Crossed above over sold level"), Alert.BAR, Sound.NoSound ); 
+######################################################
+
 
 ######################################################
 # Vertical Line Section
 ######################################################
 AddVerticalLine( if ( showTradeLines == yes && trend == trend.bull_rev &&  direction == direction.down_tick ) then yes else no, "", Color.YELLOW, Curve.FIRM );
 AddVerticalLine( if ( showTradeLines == yes && trend == trend.bear_rev &&  direction == direction.up_tick ) then yes else no, "", Color.WHITE, Curve.FIRM );
-AddVerticalLine( if ( showTradeLines == yes && Crosses( FullK, over_sold, CrossingDirection.ABOVE )) then yes else no, "", Color.DARK_GRAY, Curve.FIRM );
-AddVerticalLine( if ( showTradeLines == yes && Crosses( FullK, over_bought, CrossingDirection.BELOW )) then yes else no, "", Color.LIGHT_GRAY, Curve.FIRM );
+AddVerticalLine( if ( showTradeLines == yes && Crosses( FullK, over_sold_level, CrossingDirection.ABOVE )) then yes else no, "", Color.DARK_GRAY, Curve.FIRM );
+AddVerticalLine( if ( showTradeLines == yes && Crosses( FullK, over_bought_level, CrossingDirection.BELOW )) then yes else no, "", Color.LIGHT_GRAY, Curve.FIRM );
 ######################################################
 
+######################################################
+# Alerts for phantom buy.
+######################################################
+## Because phantom buys are dependent on coming off 
+## the first most recent over_bought high we only care 
+## about that condition.
+def last = { default none, over_bought };
+if ( FullK > over_bought_level ) {
+    last = last.over_bought;
+}
+else if ( FullK > over_sold_level && last[1] == last.over_bought ) {
+    last = last.over_bought;
+}
+else {
+    last = last.none;
+}
+
+def pb_state;
+if ( pb_state[1] == no && FullK < mid_range_level && FullK > over_sold_level && trend == trend.up && trend[1] == trend.down ) {
+    pb_state = yes;
+} else if ( pb_state[1] == yes && FullK < over_bought_level &&  FullK > over_sold_level ) {
+    pb_state = yes;
+}
+else {
+    pb_state = no;
+}
+
+## Analyze state for setting up the alert or plot.
+def trigger = { default alert_off, phantom_buy };
+if ( last == last.over_bought && pb_state[1] == no && pb_state == yes ) {
+    trigger = trigger.phantom_buy;
+}
+else {
+    trigger = trigger.alert_off;
+}
+
+Alert( trigger == trigger.phantom_buy && audibleAlerts == no, Concat( GetSymbol(), " - Phantom buy alert" ), Alert.BAR, Sound.NoSound );
+Alert( trigger == trigger.phantom_buy && audibleAlerts == yes, Concat( GetSymbol(), " - Phantom buy alert" ), Alert.BAR, Sound.Ring );
+
+AddVerticalLine( showTradeLines == yes && trigger == trigger.phantom_buy, "", Color.LIGHT_GRAY, Curve.FIRM );
+######################################################
+
+######################################################
+# Plot charateristics
+######################################################
 FullK.AssignValueColor(  if direction == direction.up_tick then Color.UPTICK else Color.DOWNTICK );
 FullD.SetDefaultColor(GetColor(4));
 FullK.SetLineWeight(2);
