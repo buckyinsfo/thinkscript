986d2e9180419a63ab9322810d763315c8460c34
buckyinsfo <buckyinsfo@gmail.com>
2017-11-17 14:49:37 -0800

    Cleaned up code.  Added switch for light or dark theme background.  Added switch to turn off weekly vertical lines. Corrected days per year to 360 for time span of volatility. Reference TastyTrade for reasoning.

--- tjs_weeklyMoveSTUDY.ts
+++ tjs_weeklyMoveSTUDY.ts
@@ -1,5 +1,6 @@
 #hint: <b>Expected Move - Weekly</b> \n This study draws the expected move levels based on implied volatility.
-#hint show_weeks: Add vertical lines to delineate weeks.
+#hint show_weeks: Add vertical lines to delineate weeks. (Values: SHOW_WKS - shows vertical line, HIDE_WKS - hides vertical line, NUM_WKS - shows verical line and weeks number label)
+#hint bkgrnd_theme: Chooose color scheme for your default background theme.
 #hint breach_notify: Add notification if the expected move is breached.
 ##################################################################################
 ##
@@ -9,7 +10,7 @@
 ##  2) The user may select whether to indicate breaches of the expected move at 
 ##     expiration.
 ##  3) Volitility for select futures contracts is derived from a corresponding
-##     ETF's volitility.
+##     ETF's volitility.`
 ##
 ##  10/15/2017 Implemented by Tim Sayre - ThinkorSwim thinkScript
 ##
@@ -18,7 +19,9 @@
 declare hide_on_intraday;
 declare upper;
 
-input show_weeks = yes;
+##input show_weeks = yes;
+input show_weeks = { default SHOW_WKS, HIDE_WKS, NUM_WKS };
+input bkgrnd_theme = { default DARK, LIGHT };
 input breach_notify = yes;
 
 # implied volatility
@@ -57,27 +60,26 @@ then {
     wk_open = open(GetSymbol());
 
     ## Friday is day 5.  So make start day zero based index
-    exp_mv =  wk_open * iv_df * Sqrt( ( 5 - ( today - 1 ) ) / 365);
+    exp_mv =  wk_open * iv_df * Sqrt( ( 5 - ( today - 1 ) ) / 360);
 
     up_lim = wk_open + exp_mv;
     down_lim = wk_open - exp_mv;
-
 } else {
-    wk_open = wk_open[1];
-    exp_mv = exp_mv[1];
-    up_lim = up_lim[1];
-    down_lim = down_lim[1];
+    wk_open = CompoundValue( 1, wk_open[1], Double.NaN );
+    exp_mv = CompoundValue( 1, exp_mv[1], Double.NaN );
+    up_lim = CompoundValue( 1, up_lim[1], Double.NaN );
+    down_lim = CompoundValue( 1, down_lim[1], Double.NaN );
 }
 
 ## Plot expected move levels.
 plot up_sd = up_lim;
 up_sd.SetPaintingStrategy( PaintingStrategy.HORIZONTAL );
-up_sd.SetDefaultColor( Color.WHITE );
+up_sd.SetDefaultColor( if bkgrnd_theme == bkgrnd_theme.DARK then Color.WHITE else Color.GRAY  );
 up_sd.SetLineWeight( 5 );
 
 plot down_sd = down_lim;
 down_sd.SetPaintingStrategy( PaintingStrategy.HORIZONTAL );
-down_sd.SetDefaultColor( Color.WHITE );
+down_sd.SetDefaultColor( if bkgrnd_theme == bkgrnd_theme.DARK then Color.WHITE else Color.GRAY ); 
 down_sd.SetLineWeight( 5 );
 
 ## Show breach.
@@ -97,8 +99,9 @@ if ( first_DOW ) then {
     total_wks = CompoundValue( 1, total_wks[1], 0 );
 }
 
-AddChartBubble( today == 5 && breach_notify && up_lim < settle, high, "Breach " + breaches, Color.YELLOW, yes );
-AddChartBubble( today == 5 && breach_notify && down_lim > settle , low, "Breach " + breaches, Color.YELLOW, no ); 
+AddChartBubble( today == 5 && breach_notify && up_lim < settle, high, "Breach " + breaches, Color.DARK_GREEN, yes );
+AddChartBubble( today == 5 && breach_notify && down_lim > settle , low, "Breach " + breaches, Color.LIGHT_GREEN, no ); 
+
 
 ## Add label if we used an EFT proxy for Volatility.
 def is_etf;
@@ -114,10 +117,12 @@ then {
     is_etf = no;
 }
 
-AddLabel( yes, Concat( breaches, Concat( " breaches in ", Concat( total_wks, Concat( " weeks.", if is_etf then " Used the ETF's volatility." else "" )))), Color.LIGHT_GRAY );
+AddLabel( breach_notify, Concat( breaches, Concat( " breaches in ", Concat( total_wks, Concat( " weeks.", if is_etf then " Used the ETF's volatility." else "" )))), Color.LIGHT_GRAY );
 
-## Display vertical line at the begining of the week.
-AddVerticalLine( first_DOW && show_weeks, total_wks, Color.WHITE, Curve.SHORT_DASH );
+## Display vertical line at the begining of each week.
+def show_lines = show_weeks == show_weeks.SHOW_WKS or show_weeks == show_weeks.NUM_WKS;
+def show_labels = show_weeks == show_weeks.NUM_WKS;
+AddVerticalLine( first_DOW && show_lines, if show_labels  then Concat( "WK - ", total_wks ) else "", Color.LIGHT_GRAY, Curve.SHORT_DASH );
 
 ## Debug Stuff.
 ##AddChartBubble( yes, high, iv, Color.LIGHT_GREEN, yes );
